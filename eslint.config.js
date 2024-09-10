b4756c8ced3c6dbf7f6f067e21e5c592db2ff6a4
Tomáš Chochola <chocholatom1997@gmail.com>
2024-09-10 19:27:53 +0200

    Use flat exports

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,6 @@
  * - Web: https://premierstacks.com
  */
 
-import { configs } from '@premierstacks/eslint-stack';
+import { ignores, browserTs, configs } from '@premierstacks/eslint-stack';
 
-export default [...configs.ignores(), ...configs.browserTs(), ...configs.configs()];
+export default [...ignores(), ...browserTs(), ...configs()];
